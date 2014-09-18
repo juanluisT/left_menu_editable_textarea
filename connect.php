bd165766c476b1db6f76fbf5cfe7640a0c83b455
Juan <juanluisT@users.noreply.github.com>
2014-09-18 15:59:36 -0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <? if(!defined('INCLUDE_CHECK')) die('You are not allowed to execute this file directly');
 
 $conexion = mysql_connect("localhost", "root", "root");
-mysql_select_db("juan", $conexion);
+mysql_select_db("DB_name", $conexion);
 ?>
